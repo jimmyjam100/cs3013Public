b9bb39746b61ef954efc72138f1926ab81443736
jimmyjam100 <jimmyjam100@aol.com>
2019-01-30 20:30:17 -0500

    made the first parts of the procAncestry.c file

---
+++ project-2/procAncestry.c
@@ -0,0 +1,35 @@
+
+#include <sys/syscall.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <string.h>
+#include <pthread.h>
+#include <assert.h>
+
+#define __NR_cs3013_syscall2 378
+
+struct ancestry {
+    pid_t ancestors[10];
+    pid_t siblings[100];
+    pid_t children[100];
+};
+
+int main(int argc, char **argv){
+    if (argc != 2 || (atoi(argv[1]) == 0 && argv[1][0] != '0')){
+        printf("please enter one paramater that is the number of a pid\n");
+        return 1;
+    }
+    struct ancestry *anc = malloc(sizeof(struct ancestry));
+    unsigned short *target_pid = malloc(sizeof(unsigned short));
+    *target_pid = atoi(argv[1]);
+    syscall(__NR_cs3013_syscall2, target_pid, anc);
+    return 0;
+
+
+
+
+}
