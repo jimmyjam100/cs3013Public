0fc5210213b5d81a7a5ccdc31d1cdf02edf86ebc
jimmyjam100 <jimmyjam100@aol.com>
2019-02-08 17:09:55 -0500

    added stuff

--- project-3/main.c
+++ project-3/main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int teams;
+
+int ninjas;
+int pirates;
+
+int nAvgCostume;
+int pAvgCostume;
+
+int nAvgArrive;
+int pAvgArrive;
+
 
 int main(int argc, char** argv) {
+    if (argc != 8 || !( 
+       (atoi(argv[1]) >= 2 && atoi(argv[1]) <= 4) && 
+       (atoi(argv[2]) >= 10 && atoi(argv[2]) <= 50) && 
+       (atoi(argv[3]) >= 10 && atoi(argv[3]) <= 50) &&
+       (atoi(argv[4]) != 0 || argv[4][0] == '0') &&
+       (atoi(argv[5]) != 0 || argv[5][0] == '0') &&
+       (atoi(argv[6]) != 0 || argv[6][0] == '0') &&
+       (atoi(argv[7]) != 0 || argv[7][0] == '0'))){
+        printf("wrong args\n");
+        return -1;
+    }
+    teams = atoi(argv[1]);
+    ninjas = atoi(argv[2]);
+    pirates = atoi(argv[3]);
+    nAvgCostume = atoi(argv[4]);
+    pAvgCostume = atoi(argv[5]);
+    nAvgArrive = atoi(argv[6]);
+    pAvgArrive = atoi(argv[7]);
+    
+    
     printf("Hello, World!\n");
     return 0;
 }
