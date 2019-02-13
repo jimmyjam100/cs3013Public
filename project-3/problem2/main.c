0ac153ae9371656834b50282d7962a48734fb326
jcharante <git@jcharante.com>
2019-02-13 09:39:02 -0500

    boilerplate

---
+++ project-3/problem2/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <bits/semaphore.h>
+#include <semaphore.h>
+#include <zconf.h>
+#include <syscall.h>
+#include <malloc.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+#define num_threads 20
+
+sem_t sem_quad_1;
+sem_t sem_quad_2;
+sem_t sem_quad_3;
+sem_t sem_quad_4;
+sem_t sem_driving_permit;
+sem_t sem_can_use_other_sems;
+
+pthread_t tids[num_threads];
+
+struct car {
+    int good_boy_points;
+    pid_t tid;
+};
+
+struct car* quad_1_intents;
+struct car* quad_2_intents;
+struct car* quad_3_intents;
+struct car* quad_4_intents;
+
+/*
+ * Quadrant Map:
+ *     North (2)
+ *    --------------
+ * W |      |       | E
+ * E |   2  |   1   | A
+ * S |      |       | S
+ * T  --------------  T
+ * ( |      |       | (
+ * 3 |  3   |   4   | 1
+ * ) |      |       | )
+ *    --------------
+ *     South (4)
+ */
+
+// returns milliseconds since the epoch
+unsigned long long getCurJSEpoch() {
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return ((unsigned long long)(t.tv_sec) * 1000 + (unsigned long long)(t.tv_usec) / 1000);
+}
+
+int generateRandomInt(int min, int max) {
+    int ret = lrand48() % max + min;
+    return ret;
+}
+
+int generateSpawnPoint() {
+    return generateRandomInt(1, 4);
+}
+
+
+void* thread(void *args) {
+    pid_t tid = syscall(SYS_gettid);
+    struct car* this_vehicle = malloc(sizeof(struct car));
+    this_vehicle->tid = tid;
+    while (1) {
+        this_vehicle->good_boy_points = 0;
+        printf("%d: Spawned\n", tid);
+    }
+}
+
+
+int main() {
+    srand(time(NULL));
+    int err, i;
+    err = sem_init(&sem_quad_1, 0, 1);
+    err = err || sem_init(&sem_quad_2, 0, 1);
+    err = err || sem_init(&sem_quad_3, 0, 1);
+    err = err || sem_init(&sem_quad_4, 0, 1);
+    err = err || sem_init(&sem_driving_permit, 0, 3);
+    err = err || sem_init(&sem_can_use_other_sems, 0, 1);
+    if (err) { // error intializing semaphores
+        return 1;
+    }
+    printf("Spawning threads\n");
+    for (i = 0; i < num_threads; i++) {
+        if (pthread_create(&tids[i]), NULL, thread, (void *) 0) {
+
+        }
+    }
+    return 0;
+}
