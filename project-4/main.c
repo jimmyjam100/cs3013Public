119a050cba4b74f13b816a4741579b906d5fdcdd
jcharante <git@jcharante.com>
2019-02-17 14:25:01 -0500

    boilerplate

--- project-4/main.c
+++ project-4/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
+    while (1) {
+        char *instruction;
+        printf("Instruction? ");
+        if (scanf("%s", instruction) == EOF) {
+            printf("\nEnd of program\n");
+        }
+        // do stuff with instructions
+    }
     printf("Hello, World!\n");
     return 0;
 }
