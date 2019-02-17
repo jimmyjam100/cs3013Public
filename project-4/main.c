b31dd92617e043521fd371893a7c8cd540fa4064
jcharante <git@jcharante.com>
2019-02-17 14:29:18 -0500

    update data entry

--- project-4/main.c
+++ project-4/main.c
@@ -2,12 +2,14 @@
 
 int main() {
     while (1) {
-        char *instruction;
+        char instruction[100];
         printf("Instruction? ");
         if (scanf("%s", instruction) == EOF) {
             printf("\nEnd of program\n");
+            return 0;
         }
         // do stuff with instructions
+        printf("%s\n", instruction);
     }
     printf("Hello, World!\n");
     return 0;
